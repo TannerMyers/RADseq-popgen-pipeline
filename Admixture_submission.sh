15895571483687bc215dd54e1293bab171de63ea
Tanner C. Myers <tannercmyers@gmail.com>
2021-04-07 17:25:04 -0500

    Adding array of Ks to iterate over

--- Admixture_submission.sh
+++ Admixture_submission.sh
@@ -6,11 +6,11 @@ INPUT_FILE=$1
 
 OUTPUT_DIR=$2
 
+# This array will be used as K values for Admixture runs
 POPULATIONS=(1 2 3 4 5 6 7 8 9 10)
 
-
-for K in $POPULATIONS; 
+# Run Admixture on a number of "K" value.
+for K in ${POPULATIONS[@]}; 
 	do
-	
-	#	admixture $INPUT_FILE $K
+	    admixture $INPUT_FILE $K
 	done
