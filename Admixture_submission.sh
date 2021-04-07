bc8b7e801a683b22739f36eab4a1260c5c8b12e0
Tanner C. Myers <tannercmyers@gmail.com>
2021-04-07 17:01:39 -0500

    Adding a script to run Admixture.
    This script will run the maximum-likelihood population structure
    estimation software Admixture on an input file provided by the user on
    the command line. Using a for loop, this iterates over a variety of "K"
    values, creating Admixture outputs to be compared for the identification
    of the optimal number of populations.

--- Admixture_submission.sh
+++ Admixture_submission.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+
+
+# Name variables
+INPUT_FILE=$1
+
+OUTPUT_DIR=$2
+
+POPULATIONS=(1 2 3 4 5 6 7 8 9 10)
+
+
+for K in $POPULATIONS; 
+	do
+	
+	#	admixture $INPUT_FILE $K
+	done
