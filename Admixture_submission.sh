e36a3bd42379cb6da17af6de8b6dbdcb50b04821
Tanner C. Myers <tannercmyers@gmail.com>
2021-04-08 12:54:07 -0500

    Changing iterants of for loop and adding flag for cross validation

--- Admixture_submission.sh
+++ Admixture_submission.sh
@@ -6,11 +6,8 @@ INPUT_FILE=$1
 
 OUTPUT_DIR=$2
 
-# This array will be used as K values for Admixture runs
-POPULATIONS=(1 2 3 4 5 6 7 8 9 10)
-
 # Run Admixture on a number of "K" value.
-for K in ${POPULATIONS[@]}; 
+for K in 1 2 3 4 5 6 7 8 9 10; 
 	do
-	    admixture $INPUT_FILE $K
+	    admixture --cv $INPUT_FILE $K
 	done
