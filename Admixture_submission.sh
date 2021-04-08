6609822076c082eda4d9c8478f2316c9560bb159
Tanner C. Myers <tannercmyers@gmail.com>
2021-04-08 13:23:00 -0500

    Adding &> to redirect standard out to a log file variable

--- Admixture_submission.sh
+++ Admixture_submission.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-
 # Name variables
 INPUT_FILE=$1
+
+LOG_FILE=$2
 
 # Run Admixture on a number of "K" value.
 for K in 1 2 3 4 5 6 7 8 9 10; 
 	do
-	    admixture --cv $INPUT_FILE $K
+	    admixture --cv $INPUT_FILE $K &> $LOG_FILE
 	done
