280548ff0e1cc6f9b798b37a5965a9d079959dd2
Tanner C. Myers <tannercmyers@gmail.com>
2021-04-09 14:59:26 -0500

    Removing bootstrap flag; I believe it's only possible with .map file

--- run_admixture.sh
+++ run_admixture.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+#conda init bash
+#conda activate structure
+
 # Name variables
 INPUT_FILE=$1
 
 # Run Admixture on a number of "K" value.
 for K in 1 2 3 4 5 6 7 8 9 10; 
 	do
-	    admixture -B20 --cv $INPUT_FILE $K -j4
+	    admixture --cv $INPUT_FILE $K | tee log${K}.out
 
 	done
