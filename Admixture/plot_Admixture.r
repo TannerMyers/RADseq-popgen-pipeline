f31be6d5ed0c488f94879f27ffa5f1e1db20dff9
Tanner C. Myers <tannercmyers@gmail.com>
2021-04-12 07:09:37 -0500

    Adding comments

--- Admixture/plot_Admixture.r
+++ Admixture/plot_Admixture.r
@@ -6,11 +6,19 @@
 # Change to your working directory
 setwd("/scratch/tcm0036/scripting-project/RADseq-popgen-pipeline/Admixture-test")
 
+# Iterate over the different .Q extension files produced by Admixture to generate
+# barplots showing ancestry
 files <- list.files(pattern=".Q")
 for (Q in files){
     output <- basename(file.path(Q, fsep=".Q"))
+    
+    # Read in the .Q file as a table
     tbl <- read.table(Q)
-    pdf(paste0("Admixture_",Q,".pdf"))
+
+    # Save plots as pdfs
+    pdf(file = paste0("Admixture_",Q,".pdf"))
+    
+    # Make structure plot using rainbow colors for different K values
     barplot(t(as.matrix(tbl)), col=rainbow(),
     xlab="Individual #", ylab="Ancestry", border=NA)
     dev.off()
